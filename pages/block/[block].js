02dae349624fa5badf566a19157bb42785c26fb9
jasontongg <aboytong123@gmail.com>
2024-09-17 23:31:59 +0700

    block detail page on progress

--- pages/block/[block].js
+++ pages/block/[block].js
@@ -21,6 +21,16 @@ import { PiCubeDuotone } from "react-icons/pi";
 import { FiFileText } from "react-icons/fi";
 import Pagination from "@mui/material/Pagination";
 import { useRouter } from "next/router";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+import Link from "next/link";
+import { PiTimerLight } from "react-icons/pi";
+import { FaCheckCircle } from "react-icons/fa";
+import { LuClock5 } from "react-icons/lu";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import { FaAngleDown } from "react-icons/fa6";
 
 const projectId = "d4e79a3bc1f5545a422926acb6bb88b8";
 
@@ -71,6 +81,13 @@ export default function BlockDetail() {
   const router = useRouter();
   const { block } = router.query;
   const [latestBlock, setLatestBlock] = useState(0);
+  const [tabActive, setTabActive] = useState(0);
+  const [expanded, setExpanded] = React.useState(false);
+  const [transaction, setTransaction] = useState([]);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   const accountDetails = async () => {
     if (block) {
@@ -79,6 +96,10 @@ export default function BlockDetail() {
         const topTenDetails = await provider.getBlock(+block);
         setTenBlockWithDetails(topTenDetails);
         setLatestBlock(latestBlock);
+
+        const blockTransaction = await provider.getBlock(getCurrentBlock);
+        setTransaction(blockTransaction.transactions);
+
         console.log(topTenDetails);
       } catch (error) {
         console.log("something went wrong...", error);
@@ -143,7 +164,7 @@ export default function BlockDetail() {
   }, [dispatch, block]);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full">
+    <div className="flex flex-col items-center w-full">
       <Navbar
         isConnected={isConnected}
         address={address}
@@ -188,49 +209,202 @@ export default function BlockDetail() {
           </div>
         </div>
       </div>
-      {tenBlockWithDetails && latestBlock && (
-        <div>
-          <p>{block}</p>
-          <p>Block Height: {tenBlockWithDetails?.number}</p>
-          <p>
-            Status:{" "}
-            {latestBlock - tenBlockWithDetails?.number > 0
-              ? "Finalized"
-              : "Unfinalized"}
-          </p>
-          <p>
-            Timestamp:{" "}
-            {tenBlockWithDetails?.timestamp
-              ? `${timeAgoFromTimestamp(tenBlockWithDetails?.timestamp)} (${
-                  tenBlockWithDetails?.date
-                })`
-              : "Loading..."}
-          </p>
-          <p>
-            Transactions: {tenBlockWithDetails?.transactions?.length}{" "}
-            transactions
-          </p>
-          <p>Fee Recipient: {tenBlockWithDetails?.miner}</p>
-          <p>
-            Total Difficulty: {(tenBlockWithDetails?.difficulty).toString()}
-          </p>
-          <p>
-            Gas Used: {Number(tenBlockWithDetails?.gasUsed)} (
-            {(
-              (Number(tenBlockWithDetails?.gasUsed) /
-                Number(tenBlockWithDetails?.gasLimit)) *
-              100
-            ).toFixed(2)}
-            %)
-          </p>
-          <p>Gas Limit: {Number(tenBlockWithDetails?.gasLimit)}</p>
-          <p>Base Fee Per Gas: {Number(tenBlockWithDetails?.baseFeePerGas)}</p>
-          <p>Hash: {tenBlockWithDetails?.hash}</p>
-          <p>Parent Hash: {tenBlockWithDetails?.parentHash}</p>
-          <p>StateRoot: {tenBlockWithDetails?.stateRoot}</p>
-          <p>ReceiptsRoot : {tenBlockWithDetails?.receiptsRoot}</p>
-          <p>Nonce: {tenBlockWithDetails?.nonce}</p>
-        </div>
+      <div className="self-start flex gap-4 items-center mt-5 mx-auto w-[95vw]">
+        <button
+          style={{
+            backgroundColor: `${
+              tabActive === 0 ? "#1c1f4f" : "rgba(229,231,235)"
+            }`,
+            color: `${tabActive === 0 ? "white" : "black"}`,
+          }}
+          className="py-2 px-4 rounded-[5px] transition-all"
+          onClick={() => setTabActive(0)}
+        >
+          Overview
+        </button>
+        <button
+          style={{
+            backgroundColor: `${
+              tabActive === 1 ? "#1c1f4f" : "rgba(229,231,235)"
+            }`,
+            color: `${tabActive === 1 ? "white" : "black"}`,
+          }}
+          className="py-2 px-4 rounded-[5px] transition-all"
+          onClick={() => setTabActive(1)}
+        >
+          Transactions
+        </button>
+      </div>
+      {tenBlockWithDetails && latestBlock ? (
+        tabActive === 0 ? (
+          <div className="flex flex-col gap-4 shadow-xl overflow-hidden rounded-[15px] p-8 my-5 mx-auto w-[95vw]">
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Block Height:</p>
+              <div className="flex gap-2 items-center">
+                <p>{tenBlockWithDetails?.number}</p>
+                <Link
+                  href={`/block/${Number(tenBlockWithDetails?.number) - 1}`}
+                  className="bg-gray-200 p-1 rounded-[5px]"
+                >
+                  <FaAngleLeft />
+                </Link>
+                <Link
+                  href={`/block/${Number(tenBlockWithDetails?.number) + 1}`}
+                  className="bg-gray-200 p-1 rounded-[5px]"
+                >
+                  <FaAngleRight />
+                </Link>
+              </div>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Status:</p>
+              <p>
+                {latestBlock - tenBlockWithDetails?.number > 0 ? (
+                  <div className="flex w-fit items-center gap-2 bg-green-100 p-1 px-3 border-[1px] border-gray-300 rounded-[2px]">
+                    <FaCheckCircle className="text-green-600" />
+                    <p className="text-green-600 font-medium">Finalized</p>
+                  </div>
+                ) : (
+                  <div className="flex w-fit items-center gap-2 bg-gray-200 p-1 px-3 border-[1px] border-gray-300 rounded-[2px]">
+                    <PiTimerLight />
+                    <p>Unfinalized</p>
+                  </div>
+                )}
+              </p>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Timestamp:</p>
+              <div className="flex gap-2 items-center">
+                <LuClock5 />
+                <p>
+                  {tenBlockWithDetails?.timestamp
+                    ? `${timeAgoFromTimestamp(
+                        tenBlockWithDetails?.timestamp
+                      )} (${tenBlockWithDetails?.date})`
+                    : "Loading..."}
+                </p>
+              </div>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Transactions:</p>
+              <p className="text-primary-deepBlue cursor-pointer font-medium">
+                {tenBlockWithDetails?.transactions?.length} transactions
+              </p>
+            </div>
+            <div className="my-[.5rem] w-[full] h-[1px] bg-gray-200"></div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Fee Recipient:</p>
+              <Link
+                href={`/account/${tenBlockWithDetails?.miner}`}
+                className="text-primary-deepBlue cursor-pointer font-medium"
+              >
+                {tenBlockWithDetails?.miner}
+              </Link>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Total Difficulty:</p>
+              <p>
+                {(tenBlockWithDetails?.difficulty)
+                  .toString()
+                  .toLocaleString("en-US")}
+              </p>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Gas Used:</p>
+              <p>
+                {Number(tenBlockWithDetails?.gasUsed).toLocaleString("en-US")} (
+                {(
+                  (Number(tenBlockWithDetails?.gasUsed) /
+                    Number(tenBlockWithDetails?.gasLimit)) *
+                  100
+                ).toFixed(2)}
+                %)
+              </p>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Gas Limit:</p>
+              <p>
+                {Number(tenBlockWithDetails?.gasLimit).toLocaleString("en-US")}
+              </p>
+            </div>
+            <div className="grid" style={{ gridTemplateColumns: "300px 1fr" }}>
+              <p className="text-[1.1rem] text-gray-600">Base Fee Per Gas:</p>
+              <div className="flex gap-2 items-center">
+                <p>
+                  {ethers.formatEther(
+                    BigInt(Number(tenBlockWithDetails?.baseFeePerGas))
+                  )}{" "}
+                  ETH
+                </p>{" "}
+                <p className="text-gray-500">
+                  (
+                  {ethers.formatUnits(
+                    BigInt(Number(tenBlockWithDetails?.baseFeePerGas)),
+                    "gwei"
+                  )}{" "}
+                  Gwei)
+                </p>
+              </div>
+            </div>
+            <Accordion
+              expanded={expanded === "panel4"}
+              onChange={handleChange("panel4")}
+            >
+              <AccordionSummary
+                expandIcon={<FaAngleDown />}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                  Click to show more
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div
+                  className="grid"
+                  style={{ gridTemplateColumns: "300px 1fr" }}
+                >
+                  <p className="text-[1.1rem] text-gray-600">Hash:</p>
+                  <p>{tenBlockWithDetails?.hash}</p>
+                </div>
+                <div
+                  className="grid"
+                  style={{ gridTemplateColumns: "300px 1fr" }}
+                >
+                  <p className="text-[1.1rem] text-gray-600">Parent Hash:</p>
+                  <p>{tenBlockWithDetails?.parentHash}</p>
+                </div>
+                <div
+                  className="grid"
+                  style={{ gridTemplateColumns: "300px 1fr" }}
+                >
+                  <p className="text-[1.1rem] text-gray-600">StateRoot:</p>
+                  <p>{tenBlockWithDetails?.stateRoot}</p>
+                </div>
+                <div
+                  className="grid"
+                  style={{ gridTemplateColumns: "300px 1fr" }}
+                >
+                  <p className="text-[1.1rem] text-gray-600">ReceiptsRoot:</p>
+                  <p>{tenBlockWithDetails?.receiptsRoot}</p>
+                </div>
+                <div
+                  className="grid"
+                  style={{ gridTemplateColumns: "300px 1fr" }}
+                >
+                  <p className="text-[1.1rem] text-gray-600">Nonce:</p>
+                  <p>{tenBlockWithDetails?.nonce}</p>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-4 shadow-xl overflow-hidden rounded-[15px] p-8 my-5 mx-auto w-[95vw]">
+            Transaction
+          </div>
+        )
+      ) : (
+        "Loading..."
       )}
     </div>
   );
